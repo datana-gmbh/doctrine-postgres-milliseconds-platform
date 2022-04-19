8ad4d899e68e28cab2ce8cadf4fe8d0110c4e71d
Oskar Stark <oskarstark@googlemail.com>
2022-04-19 22:27:49 +0200

    Fix: CS

--- tests/Unit/Doctrine/DBAL/Platforms/PostgreSQLMillisecondsPlatformTest.php
+++ tests/Unit/Doctrine/DBAL/Platforms/PostgreSQLMillisecondsPlatformTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of datana-gmbh/doctrine-postgresql-milliseconds-platform package.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Datana\Doctrine\PostgreSQL\Platform\Tests\Unit\Doctrine\DBAL\Platforms;
 
 use Datana\Doctrine\PostgreSQL\Platform\Doctrine\DBAL\Platforms\PostgreSQLMillisecondsPlatform;
